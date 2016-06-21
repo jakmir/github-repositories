826434005b332b99b16e5785629f3eeb1d51656d
JakmirInsight <jakmir.net@gmail.com>
2016-06-21 16:31:47 +0300

    Initial Commit

---
+++ GithubRepositories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubRepositories
+//
+//  Created by Jakmir on 6/21/16.
+//  Copyright © 2016 Jakmir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubRepositories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubRepositories
+//
+//  Created by Jakmir on 6/21/16.
+//  Copyright © 2016 Jakmir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
